dff269d93628eb26e171edaf5148e69a79a8e04e
z0s3r77 <ipopdue24@gmail.com>
2023-01-18 12:52:05 +0100

    feat(gilded_rose):Añadida clase GildedRose Se ha añadido la clase GildedRose en el modulo gilded_rose, con el fin de poder pasar los casos test de Emily en texttest_fixture

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -1,3 +1,39 @@
+class GildedRose: 
+    def __init__(self, items):
+        self.items = items
+        self.objects = []
+        self.avaliableObjects = {
+            "AgedBrie":"Aged Brie",
+            "Sulfuras":"Sulfuras, Hand of Ragnaros",
+            "Backstage":"Backstage passes to a TAFKAL80ETC concert",
+            "Conjured":"Conjured Mana Cake"
+        }
+
+    def getObjects(self):
+        return self.objects
+
+
+    #Esta función nos crea una lista con los objetos de los items de Emily filtrados.
+    #Aunque todos sean Items, en este codigo, se han hecho subclases por cada tipo de item
+    #Esas subclases són los objetos disponibles en el diccionario self.avaliableObjects.
+    def setObjects(self):
+        for item in self.items:
+            if item.name in self.avaliableObjects.values():
+                for i in self.avaliableObjects:
+                    if self.avaliableObjects[i] == item.name:
+                        value = i
+                self.objects.append([eval(value)(item.name, item.sell_in, item.quality)])
+            else:
+                self.objects.append([NormalItem(item.name, item.sell_in, item.quality)])
+
+
+
+    def update_quality(self):
+        for item in self.items:
+            item[0].updateQuality()
+
+
+
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
---
+++ test/texttest_fixture.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+
+from src.gilded_rose import *
+
+if __name__ == "__main__":
+    print ("OMGHAI!")
+    items = [
+             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+             Item(name="Aged Brie", sell_in=2, quality=0),
+             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+             Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+             Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+             Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+            ]
+    
+
+    # Parche improvisado para hacer que siga funcionando mi codigo 
+    # Con el test de EmilyBache
+    itemsToParticularObject = GildedRose(items)
+    itemsToParticularObject.setObjects()
+    items = itemsToParticularObject.getObjects()
+
+
+
+    days = 10
+    import sys
+    if len(sys.argv) > 1:
+        days = int(sys.argv[1]) + 1
+    for day in range(days):
+        print("-------- day %s --------" % day)
+        print("name, sellIn, quality")
+        for item in items:
+            print(item[0])
+        print("")
+        GildedRose(items).update_quality()
+
+
+
+
+
